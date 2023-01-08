ee0cba9ed68129fa47b4c1d81b20f468e2eddbb1
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2023-01-08 15:09:11 +0400

    practic

--- 301122.py
+++ 301122.py
@@ -3,11 +3,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/teo/')
-def teo():
-    return render_template('teo.html')
+@app.route('/kawasaki/')
+def kawasaki():
+    return render_template('kawasaki.html')
 @app.route('/honda/')
 def honda():
     return render_template('honda.html')
+@app.route('/practice/')
+def practice():
+    return render_template('practice.html')
 if __name__ == '__main__':
     app.run()
